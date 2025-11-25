8aa780115b4e78c6100d16c55922adaa348aaead
theunrealhulk <chafiaay.abj@gmail.com>
2025-11-25 16:32:14 +0000

    add dark theme toggle

--- python101/textual/stopwatch.py
+++ python101/textual/stopwatch.py
@@ -3,15 +3,21 @@ from textual.widgets import Header, Footer
 
 
 class StopWatchApp(App):
+
+    dark_mode = False  # track custom dark mode
+
     # required for running the app from vscode
     BINDINGS = [
         ("q", "quit", "Quit"), #quit app with 'q' instead of 'CTRL+Q'
+        ("d", "toggle_dark_mode", "toggle Dark Mode"), #quit app with 'q' instead of 'CTRL+Q'
     ]
 
     def compose(self):
         yield Header(show_clock=True)
         yield Footer()
 
+    def action_toggle_dark_mode(self):
+        self.theme = "textual-dark" if self.theme == "textual-light" else "textual-light"
 
 
 if __name__=="__main__":
